a6f4220eb2f5921021938a73bbad437ff67c14e8
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-10-07 11:40:30 +0200

    added comments

--- Sources/GFSwiftUtilities/UIViewController+ext.swift
+++ Sources/GFSwiftUtilities/UIViewController+ext.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+/// Extention to UIViewController exposing customAdd and customRemoveFromParent
 @nonobjc extension UIViewController {
+    /// Adds a child to the view controller
+    /// and sets an optional frame to the child's view
+    /// The child's view is added to the VC view
+    /// and didMove(toParent) is called on the child
+    /// - Parameters:
+    ///   - child: The child view controller to add
+    ///   - frame: an optional frame to set to the child view
     func customAdd(_ child: UIViewController, frame: CGRect? = nil) {
-        child.modalPresentationStyle = .fullScreen
         addChild(child)
 
         if let frame = frame {
@@ -19,7 +26,11 @@ import UIKit
         view.addSubview(child.view)
         child.didMove(toParent: self)
     }
-
+    
+    /// Removes a view controller from its parent
+    /// willMove(toParent) is called, then the view
+    /// is removed from its superview and finally
+    /// removeFromParent is called
     func customRemoveFromParent() {
         willMove(toParent: nil)
         view.removeFromSuperview()
